d0448554e8340f4f40f8e53860d1d3e8757f84e7
Vennalareddy16 <vennalareddy1626@gmail.com>
2026-04-19 23:53:27 +0530

    config: Boilerplate

--- script.js
+++ script.js
@@ -1,28 +1,39 @@
-const doMath=(n1,n2,operator)=>{
+const doMath=(operator)=>{
+        let n1=Number(document.getElementById("n1").value);
+        let n2=Number(document.getElementById("n2").value);
+        let result="";
     switch(operator){
     case '+':
-            console.log(`The Addition of ${n1} and ${n2} is ${n1+n2}`);
+           // console.log(`The Addition of ${n1} and ${n2} is ${n1+n2}`); 
+            result=`The Addition of ${n1} and ${n2} is ${n1+n2}`;
             break;
             
     case '-':
-            console.log(`The Subtraction of ${n1} and ${n2} is ${n1-n2}`);
+            //console.log(`The Subtraction of ${n1} and ${n2} is ${n1-n2}`);
+            result=`The Subtraction of ${n1} and ${n2} is ${n1-n2}`
             break;
 
     case '*':
-                console.log(`The Multiplication of ${n1} and ${n2} is ${n1*n2}`);
+                //console.log(`The Multiplication of ${n1} and ${n2} is ${n1*n2}`);
+                 result=`The Multiplication of ${n1} and ${n2} is ${n1*n2}`
                 break;
     case '/': 
-                console.log(`The Division of ${n1} and ${n2} is ${n1/n2}`);
+                //console.log(`The Division of ${n1} and ${n2} is ${n1/n2}`);
+                result=`The Division of ${n1} and ${n2} is ${n1/n2}`
                 break;
     case '%':
-                console.log(`The Modulus of ${n1} and ${n2} is ${n1%n2}`);
+                //console.log(`The Modulus of ${n1} and ${n2} is ${n1%n2}`);
+                result=`The Modulus of ${n1} and ${n2} is ${n1%n2}`
                 break;
     case '**':
-                console.log(`The Power of ${n1} and ${n2} is ${n1**n2}`);
+                //console.log(`The Power of ${n1} and ${n2} is ${n1**n2}`);
+                result=`The Power of ${n1} and ${n2} is ${n1**n2}`
                 break;
     default:
-            console.log("Invalid Operator") 
+            //console.log("Invalid Operator") 
+                result="Invalid Operator"
             break;
     }
+    document.getElementById("result").innerText=result;
 }
-doMath(2,4,"-");
+//doMath(2,4,"-");
